d9c6495958bc022d5abf2e6f46d6d9505aef9d52
aean0x <3682177+aean0x@users.noreply.github.com>
2024-08-23 20:57:43 +0200

    221 current  2024-08-23 20:51:45  24.11.20240821.c374d94  6.6.47                          *

--- configuration.nix
+++ configuration.nix
@@ -149,8 +149,7 @@ in {
   ];
 
   # udev rules
-  services.ude
-  v.extraRules = ''
+  services.udev.extraRules = ''
     ACTION=="add", ATTR{idVendor}=="046d", ATTR{idProduct}=="c548", TEST=="power/wakeup", ATTR{power/wakeup}="disabled"
     ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{remove}="1"
   ''; # Disable USB mouse wake up because my fucking logitech mouse randomly wakes up computer
