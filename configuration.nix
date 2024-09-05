fe6b680b607c8cc69e19485dbecb47a9bb9f90d9
aean0x <3682177+aean0x@users.noreply.github.com>
2024-09-05 11:58:36 +0200

    237 current  2024-09-05 11:58:30  24.11.20240831.12228ff  6.6.48                          *

--- configuration.nix
+++ configuration.nix
@@ -241,10 +241,10 @@ in {
     # enableNvidia = true;
     # rootless.enable = true;
     # rootless.setSocketVariable = true;
-    # daemon.settings = {
-    #   default-runtime = "nvidia";
-    #   # runtimes.nvidia.path = "${pkgs.nvidia-container-toolkit}/bin/nvidia-container-runtime";
-    # };
+    daemon.settings = {
+      default-runtime = "nvidia";
+      # runtimes.nvidia.path = "${pkgs.nvidia-container-toolkit}/bin/nvidia-container-runtime";
+    };
     # extraPackages = with pkgs; [
     #   nvidia-container-toolkit
     # ];
