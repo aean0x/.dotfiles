f858c99a95bab8a4c37a70941ee5ef83ebc75eb4
aean0x <3682177+aean0x@users.noreply.github.com>
2024-09-05 12:58:57 +0200

    245 current  2024-09-05 12:58:51  24.11.20240831.12228ff  6.6.48                          *

--- configuration.nix
+++ configuration.nix
@@ -113,7 +113,6 @@ in {
   #   wantedBy = ["multi-user.target"];
   #   after = ["docker.service" "docker.socket"];
   # };
-  systemd.enableUnifiedCgroupHierarchy = false;
 
   # Boot settings
   boot = {
@@ -240,13 +239,13 @@ in {
   virtualisation.docker = {
     enable = true;
     enableOnBoot = true;
-    # enableNvidia = true;
+    enableNvidia = true;
     # rootless.enable = true;
     # rootless.setSocketVariable = true;
-    # daemon.settings = {
-    #   default-runtime = "nvidia";
-    #   # runtimes.nvidia.path = "${pkgs.nvidia-container-toolkit}/bin/nvidia-container-runtime";
-    # };
+    daemon.settings = {
+      default-runtime = "nvidia";
+      # runtimes.nvidia.path = "${pkgs.nvidia-container-toolkit}/bin/nvidia-container-runtime";
+    };
     # extraPackages = with pkgs; [
     #   nvidia-container-toolkit
     # ];
