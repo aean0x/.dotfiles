b8bc6fa8194377a67e99860f2c61464f4c53fad3
aean0x <3682177+aean0x@users.noreply.github.com>
2025-03-31 12:15:06 +0200

    406 current  2025-03-31 12:15:04  25.05.20250322.1e5b653  6.13.7                          *

--- flake.nix
+++ flake.nix
@@ -46,7 +46,7 @@
         {nixpkgs.overlays = overlays;}
         ./configuration.nix
         ./hardware-configuration.nix
-        ./aerotheme/system.nix
+        #./aerotheme/system.nix
         flatpaks.nixosModules.nix-flatpak
         home-manager.nixosModules.home-manager
         {
@@ -59,7 +59,7 @@
             imports = [
               ./home/home.nix
               ./home/plasma.nix
-              ./aerotheme/user.nix
+              #./aerotheme/user.nix
               flatpaks.homeManagerModules.nix-flatpak
             ];
           };
